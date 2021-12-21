1a7fa807fad8667778ea8d88613570ca15cfff99
elliehachemjob <elliehachemjob@gmail.com>
2021-12-21 12:52:19 +0200

    resturant

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
